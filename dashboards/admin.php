2e45d1ffc279de939466e4345a0b810765fba3b5
Abdulwahid-Palanggalan <ojt@sfgroupcompanies.com>
2026-02-23 17:18:07 +0800

    center

--- dashboards/admin.php
+++ dashboards/admin.php
@@ -333,7 +333,7 @@ $baseUrl = $GLOBALS['base_url'];
         <div class="row">
             <?php include_once $rootPath . '/includes/sidebar.php'; ?>
             
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4 main-content">
+            <main class="ms-sm-auto px-md-4 main-content">
                 <!-- Dashboard Header -->
                 <div class="dashboard-header">
                     <div class="row align-items-center">
